be5ddecf118434e08f510875840b622862eac945
Wesley <wesley.felix12@hotmail.com>
2023-04-02 16:40:13 -0300

    typescript add

--- src/Site.jsx
+++ src/Site.jsx
@@ -97,6 +97,7 @@ const Site = () => {
         <img className='langs' height="65" width="65" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="Javascript"/>
         <img className='langs' height="65" width="65" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React"/>
         <img className='langs' height="65" width="65" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="Git" />
+        <img className='langs' height="65" width="65" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="Typescript" />
         <LanguagesExamples className='lang-reposit'>
           <LanguagesExamplesTitle>Exemplos do que sei fazer em tais linguagens:</LanguagesExamplesTitle>
           <LanguagesExamplesDescrips><LanguagesExamplesLang>HTML: </LanguagesExamplesLang>Criação de divs, navbar, footer, select/options e tabelas.</LanguagesExamplesDescrips>
@@ -104,6 +105,7 @@ const Site = () => {
           <LanguagesExamplesDescrips><LanguagesExamplesLang>Javascript: </LanguagesExamplesLang>Criação de variáveis, funções, loops, condições, classes, manipulação do DOM e assíncrono.</LanguagesExamplesDescrips>
           <LanguagesExamplesDescrips><LanguagesExamplesLang>ReactJS: </LanguagesExamplesLang>Utilização de JSX, imports, props e hooks(useState, useEffect, useContext...)</LanguagesExamplesDescrips>
           <LanguagesExamplesDescrips><LanguagesExamplesLang>Git: </LanguagesExamplesLang>Commits, branchs, e deploy no Github.</LanguagesExamplesDescrips>
+          <LanguagesExamplesDescrips><LanguagesExamplesLang>Typescript: </LanguagesExamplesLang>Tipos, enums e interfaces.</LanguagesExamplesDescrips>
         </LanguagesExamples>
       </Languages>
 
